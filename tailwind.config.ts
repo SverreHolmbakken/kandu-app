13b620c0d0a4891ef18362d68f4bc240fd73ed4c
Øyvind Gjersum <110482909+0yvz@users.noreply.github.com>
2023-10-02 15:34:22 +0200

    Feature/install shadcn (#5)
    * Installed shadcn
    
    * Made example button with UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,36 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
 	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-			},
-			colors: {
-				primaryDark: '#191717',
-				primaryLight: '#F1EFEF',
-			},
-			screens: {
-				small: '480px',
-				medium: '768',
-				large: '1028',
-			},
-			padding: {
-				small: 'p-5',
-				medium: 'p-10',
-				large: 'p-20',
-			},
-		},
-	},
-	plugins: [],
-};
-export default config;
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
