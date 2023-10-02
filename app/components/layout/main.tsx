06eb07d5d5e4770ed39daffd93713e80be01edc5
0yvz <oyvindg@live.no>
2023-10-02 15:18:51 +0200

    Made example button with UI

--- app/components/layout/main.tsx
+++ app/components/layout/main.tsx
@@ -1,7 +1,11 @@
+import { Button } from "../ui/button";
+
 export default function Main() {
-	return (
-		<div className='bg-slate-200 h-full w-full p-10'>
-			<h1>Main</h1>
-		</div>
-	);
+  return (
+    <div className="bg-slate-200 h-full w-full p-10">
+      <h1>Main</h1>
+
+      <Button> Click me </Button>
+    </div>
+  );
 }
