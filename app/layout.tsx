546509164ef79322bd3b51cf1825a09c829c25eb
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-10-04 15:18:21 +0200

    Made header universal for all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "./components/layout/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
 	return (
 		<ClerkProvider>
 			<html lang="en">
-				<body className={inter.className}>{children}</body>
+				<body className={inter.className}>
+					<Header />
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Main from "./components/layout/main";
 export default function Home() {
 	return (
 		<div className="flex h-screen flex-col justify-between m-auto text-primaryDark">
-			<Header />
 			<section className="flex w-full px-36 h-screen">
 				<Aside />
 				<Main />
