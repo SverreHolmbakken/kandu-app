d5cc2eea2c17133db0e0aaeccee341e385f042b6
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-10-04 11:00:00 +0200

    Added button variants

--- app/components/layout/main.tsx
+++ app/components/layout/main.tsx
@@ -1,11 +1,17 @@
 import { Button } from "../ui/button";
 
 export default function Main() {
-  return (
-    <div className="bg-slate-200 h-full w-full p-10">
-      <h1>Main</h1>
+	return (
+		<div className="bg-slate-200 h-full w-full p-10">
+			<h1>Main</h1>
 
-      <Button> Click me </Button>
-    </div>
-  );
+			<Button> default </Button>
+			<Button variant="destructive"> destructive </Button>
+			<Button variant="outline"> outline </Button>
+			<Button variant="secondary"> secondary </Button>
+			<Button variant="ghost"> ghost </Button>
+			<Button variant="link"> link </Button>
+			<Button variant="gradient"> gradient </Button>
+		</div>
+	);
 }
