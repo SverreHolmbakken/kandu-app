77f1258b7eaa640d56a0d371ab065dd3f682765e
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-10-04 14:39:24 +0200

    Feature/header (#12)
    * Installed skeleton component from shadcn
    
    * Imported Kandu logo
    
    * Header component
    
    * Adjusted landing page to fit header

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import Header from './components/layout/header';
-import Aside from './components/layout/aside';
-import Main from './components/layout/main';
+import Header from "./components/layout/header";
+import Aside from "./components/layout/aside";
+import Main from "./components/layout/main";
 
 export default function Home() {
 	return (
-		<main className='flex h-screen flex-col justify-between max-w-7xl m-auto text-primaryDark'>
+		<div className="flex h-screen flex-col justify-between m-auto text-primaryDark">
 			<Header />
-			<section className='flex w-full h-screen'>
+			<section className="flex w-full px-36 h-screen">
 				<Aside />
 				<Main />
 			</section>
-		</main>
+		</div>
 	);
-};
+}
