084216adae1ffc21b804e325b372c7dca6c48c8e
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-10-04 14:33:42 +0200

    Header component

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,8 +1,40 @@
+"use client";
+
+import { ClerkLoaded, ClerkLoading, UserButton, useAuth } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import { Skeleton } from "../ui/skeleton";
+
 function Header() {
+	const { isSignedIn } = useAuth();
+
 	return (
-		<div className='flex flex-row w-full justify-between py-8 bg-red-300'>
-			<h1>KANDU</h1>
-			<button>SIGN IN</button>
+		<div className="flex flex-row w-full justify-between items-center py-2 px-36 bg-transparent">
+			<a href="/" className="flex flex-row items-center gap-6">
+				<img src="/kandu-logo.svg" alt="" />
+				<h1 className="text-slate-900 font-bold text-3xl">Kandu</h1>
+			</a>
+
+			{!isSignedIn && (
+				<Button>
+					<a href="/sign-in">Sign in</a>
+				</Button>
+			)}
+
+			{isSignedIn && (
+				<div className="flex flex-row items-center gap-8">
+					<Button variant="link">
+						<a href="/dashboard">Dashboard</a>
+					</Button>
+
+					<ClerkLoading>
+						<Skeleton className="h-8 w-8 bg-slate-400 rounded-full" />
+					</ClerkLoading>
+
+					<ClerkLoaded>
+						<UserButton />
+					</ClerkLoaded>
+				</div>
+			)}
 		</div>
 	);
 }
