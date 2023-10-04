292ccebeeb34addd5b34af9dc50e6bd3d85ba750
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-10-04 15:23:37 +0200

    Updated header to be universal for all pages (#14)
    * Updated links
    
    * Made header universal for all pages

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -3,27 +3,29 @@
 import { ClerkLoaded, ClerkLoading, UserButton, useAuth } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import { Skeleton } from "../ui/skeleton";
+import Link from "next/link";
 
 function Header() {
 	const { isSignedIn } = useAuth();
 
 	return (
 		<div className="flex flex-row w-full justify-between items-center py-2 px-36 bg-transparent">
-			<a href="/" className="flex flex-row items-center gap-6">
+			<Link href="/" className="flex flex-row items-center gap-6">
 				<img src="/kandu-logo.svg" alt="" />
 				<h1 className="text-slate-900 font-bold text-3xl">Kandu</h1>
-			</a>
+			</Link>
 
 			{!isSignedIn && (
 				<Button>
-					<a href="/sign-in">Sign in</a>
+					<Link href="/sign-in">Sign in</Link>
+
 				</Button>
 			)}
 
 			{isSignedIn && (
 				<div className="flex flex-row items-center gap-8">
 					<Button variant="link">
-						<a href="/dashboard">Dashboard</a>
+						<Link href="/dashboard">Dashboard</Link>
 					</Button>
 
 					<ClerkLoading>
