51a7984ba2c27a584fcaf6f5ee13e3020304b9b4
sergei <ravinski.sergei@gmail.com>
2023-10-05 09:13:57 +0200

    Imported components to the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,16 @@
+import Header from "../components/layout/header";
+import CreateProjectButton from "../components/ui/create-project-button";
+
 export default function Dashboard() {
 	return (
-		<section className='flex h-screen flex-col justify-between max-w-7xl m-auto'>
-			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-				Eligendi libero sed debitis distinctio soluta est magnam?
-				Corporis incidunt sequi molestias voluptatibus at nemo eveniet
-				repudiandae et ut architecto dolores error deserunt dolorem nisi
-				earum mollitia voluptates, libero vero minus, obcaecati odit
-				expedita porro iure magnam! Non dicta commodi accusantium minus
-				nam optio fuga, repellendus inventore dolorem cupiditate ut.
-				Eum, cum.
-			</p>
-		</section>
+		<div className="flex h-screen flex-col justify-between max-w-7xl m-auto">
+			<Header />
+			<main className="flex h-full flex-col text-primaryDark">
+				<h1 className="w-full text-extraLargeFont py-largePadding">
+					Projects
+				</h1>
+				<CreateProjectButton />
+			</main>
+		</div>
 	);
 }
