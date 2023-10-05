3a84a7af300f0a22cf71d1ec62a64f6e426c6e66
sergei <ravinski.sergei@gmail.com>
2023-10-05 09:11:20 +0200

    Created the create-project-button component

---
+++ app/components/ui/create-project-button.tsx
@@ -0,0 +1,11 @@
+import { Plus } from "lucide-react";
+
+export default function CreateProjectButton() {
+	return (
+		<button className="flex rounded-md w-1/5 p-extraLargePadding place-content-center border-solid border-[1px] border-primaryDark opacity-50 hover:opacity-40">
+			<div className="block w-5 h-5">
+				<Plus />
+			</div>
+		</button>
+	);
+}
