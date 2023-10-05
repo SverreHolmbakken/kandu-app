ba1d3d34545c374264a98652cc9440f4422120c0
sergei <ravinski.sergei@gmail.com>
2023-10-05 09:30:19 +0200

    Changed padding to the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import Header from "../components/layout/header";
 import CreateProjectButton from "../components/ui/create-project-button";
 
 export default function Dashboard() {
 	return (
-		<div className="flex h-screen flex-col justify-between max-w-7xl m-auto">
-			<Header />
+		<div className="flex h-screen flex-col justify-between px-36 m-auto">
 			<main className="flex h-full flex-col text-primaryDark">
 				<h1 className="w-full text-extraLargeFont py-largePadding">
 					Projects
