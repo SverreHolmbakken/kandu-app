20b923f3d71fc66ac2328f4afabf4d1341b9f10e
sergei <ravinski.sergei@gmail.com>
2023-10-05 09:29:30 +0200

    Added margin-top to the button

--- app/components/ui/create-project-button.tsx
+++ app/components/ui/create-project-button.tsx
@@ -2,7 +2,7 @@ import { Plus } from "lucide-react";
 
 export default function CreateProjectButton() {
 	return (
-		<button className="flex rounded-md w-1/5 p-extraLargePadding place-content-center border-solid border-[1px] border-primaryDark opacity-50 hover:opacity-40">
+		<button className="flex rounded-md w-1/5 p-extraLargePadding mt-10 place-content-center border-solid border-[1px] border-primaryDark opacity-50 hover:opacity-40">
 			<div className="block w-5 h-5">
 				<Plus />
 			</div>
