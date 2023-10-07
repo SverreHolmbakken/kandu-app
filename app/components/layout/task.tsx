8d47baf2a53d0c240b9fe3854940aad7251a9514
0yvz <oyvindg@live.no>
2023-10-07 18:38:23 +0200

    Adjusted task card

--- app/components/layout/task.tsx
+++ app/components/layout/task.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Edit } from "lucide-react";
 import {
   Card,
   CardHeader,
@@ -12,7 +13,16 @@ export default function Task() {
   return (
     <Card className="w-full">
       <CardHeader>
-        <CardTitle>Task title</CardTitle>
+        <CardTitle className="">
+          <div className="flex items-center justify-between text-base">
+            <div className="flex items-center">
+              <div className="mr-2">Task name</div>
+            </div>
+            <div className="ml-2">
+              <Edit className="w-5 stroke-slate-500" />
+            </div>
+          </div>
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <p>Task description</p>
