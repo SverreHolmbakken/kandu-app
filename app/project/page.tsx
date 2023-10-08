4267d653198db2205f7a9dfc02152b28aae89d56
Øyvind Gjersum <110482909+0yvz@users.noreply.github.com>
2023-10-08 15:22:38 +0200

    Feature/create kanban board layout (#21)
    * Installed table
    
    * Installed card dropdown and table ui elements
    
    * Created edit fucntion for column
    
    * Created kanban column
    
    * Created kanban layout
    
    * Created task component
    
    * Added kanban layout to page
    
    * Removed unused imports
    
    * Adjusted table color
    
    * Adjusted task card
    
    * Update kanban-column.tsx
    
    Made EditColumn button on the same line
    
    ---------
    
    Co-authored-by: Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,5 +1,11 @@
 import ProjectNav from "../components/layout/project-nav";
+import KanbanLayout from "../components/layout/kanban-layout";
 
 export default function Project() {
-  return <ProjectNav />;
+  return (
+    <>
+      <ProjectNav />
+      <KanbanLayout />
+    </>
+  );
 }
