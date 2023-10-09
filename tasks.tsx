87215f9bb0d2c0c2f2809996122fd260422a53a7
sergei <ravinski.sergei@gmail.com>
2023-10-09 10:33:16 +0200

    Created array with dummy data

---
+++ tasks.tsx
@@ -0,0 +1,30 @@
+export const tasks = [
+	{
+		id: 1,
+		title: "Footer",
+		description: "sdgbsfgbsfgbsfdb",
+		date: "16 oktober 2023",
+	},
+	{
+		id: 2,
+		title: "Main",
+		description: "sdgbsfgbsfgbsfdb",
+		date: "16 oktober 2023",
+	},
+	{
+		id: 3,
+		title: "Header",
+		description: "sdgbsfgbsfgbsfdb",
+		date: "16 oktober 2023",
+	},
+	{
+		id: 3,
+		title: "Header",
+		description: "sdgbsfgbsfgbsfdb",
+		date: "16 oktober 2023",
+	},
+];
+
+export function getAllTasks() {
+	return tasks;
+}
