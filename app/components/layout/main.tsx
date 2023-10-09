5437d3f839ffd1de558517fa72aa7d08343b8015
sergei <ravinski.sergei@gmail.com>
2023-10-09 10:41:18 +0200

    Created temporary link to the project page

--- app/components/layout/main.tsx
+++ app/components/layout/main.tsx
@@ -1,10 +1,11 @@
 import { Button } from "../ui/button";
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Main() {
 	return (
-		<div className="bg-slate-200 h-full w-full p-10">
-			<h1>Main</h1>
+		<div className="bg-slate-200 h-full w-full p-10 flex justify-between">
+			<Link href="/project">Project</Link>
 
 			<Button> default </Button>
 			<Button variant="destructive"> destructive </Button>
