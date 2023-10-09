b1a6568c264215d392b8aff0458790c8c473776b
Sergei_ravinski <Ravinski.sergei@gmail.com>
2023-10-09 11:09:58 +0200

    Feature/project button (#24)
    * Created array with dummy data
    
    * Added media queries
    
    * Updated supabase
    
    * Created temporary link to the project page
    
    * Created the ProjectCard component
    
    * Created the CreateProjectCard component
    
    * Renamed the CreateProjectButton component
    
    * Created array with colors
    
    * Created the shuffleColors function
    
    * Imported components
    
    * Added elements to the Dasboard page

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
 
 	return (
 		<div className="flex h-screen flex-col justify-between m-auto text-primaryDark">
-			<section className="flex w-full px-36 h-screen">
+			<section className="flex w-full lg:px-36 md:px-8 px-1 h-screen">
 				<Aside />
 				<Main />
 				<div>
