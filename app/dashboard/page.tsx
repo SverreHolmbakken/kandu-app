005b203a7e580a37e7e82b5a627eef2a78a1f15d
sergei <ravinski.sergei@gmail.com>
2023-10-09 10:50:11 +0200

    Created array with colors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,25 @@
 import CreateProjectButton from "../components/ui/create-project-button";
 
 export default function Dashboard() {
+
+	const colors = [
+		{
+			id: 1,
+			color: "#FF8080",
+		},
+		{
+			id: 2,
+			color: "#FFCF96",
+		},
+		{
+			id: 3,
+			color: "#F6FDC3",
+		},
+		{
+			id: 4,
+			color: "#CDFAD5",
+		},
+	];
 	return (
 		<div className="flex h-screen flex-col justify-between px-36 m-auto">
 			<main className="flex h-full flex-col text-primaryDark">
