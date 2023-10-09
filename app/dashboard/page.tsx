a152f1bc52f122fc1a464089bce53897e24433fc
sergei <ravinski.sergei@gmail.com>
2023-10-09 10:53:50 +0200

    Added elements to the Dasboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import CreateProjectCard from "../components/ui/create-project-card";
 import ProjectCard from "../components/ui/project-card";
 
 export default function Dashboard() {
+	const tasks = getAllTasks();
 
 	const colors = [
 		{
@@ -30,12 +31,22 @@ export default function Dashboard() {
 	shuffleColors();
 
 	return (
-		<div className="flex h-screen flex-col justify-between px-36 m-auto">
+		<div className="flex h-screen flex-col justify-between lg:px-36 md:px-8 px-1 m-auto">
 			<main className="flex h-full flex-col text-primaryDark">
 				<h1 className="w-full text-extraLargeFont py-largePadding">
 					Projects
 				</h1>
-				<CreateProjectButton />
+				<div className="grid grid-cols-1 grid-flow-row auto-cols-max gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+					{tasks.map((task) => (
+						<ProjectCard
+							key={task.id}
+							title={task.title}
+							description={task.description}
+							date={task.date}
+						/>
+					))}
+					<CreateProjectCard />
+				</div>
 			</main>
 		</div>
 	);
