148d35b4a77b0e5bd45094e1eeded140f1b7f7ce
0yvz <oyvindg@live.no>
2023-10-11 14:43:07 +0200

    Added theme button to header

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -4,6 +4,7 @@ import { ClerkLoaded, ClerkLoading, UserButton, useAuth } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import { Skeleton } from "../ui/skeleton";
 import Link from "next/link";
+import { ModeToggle } from "./themebutton";
 
 function Header() {
 	const { isSignedIn } = useAuth();
@@ -12,30 +13,35 @@ function Header() {
 		<div className="flex flex-row w-full justify-between items-center py-2 lg:px-36 md:px-8 px-1 bg-transparent">
 			<Link href="/" className="flex flex-row items-center gap-6">
 				<img src="/kandu-logo.svg" alt="" />
-				<h1 className="text-primaryDark font-bold text-3xl">Kandu</h1>
+				<h1 className="text-primaryDark font-bold text-3xl dark:text-white">
+					Kandu
+				</h1>
 			</Link>
 
-			{!isSignedIn && (
-				<Button>
-					<Link href="/sign-in">Sign in</Link>
-				</Button>
-			)}
-
-			{isSignedIn && (
-				<div className="flex flex-row items-center gap-8">
-					<Button variant="link">
-						<Link href="/dashboard">Dashboard</Link>
+			<div className="flex justify-end gap-8">
+				{!isSignedIn && (
+					<Button>
+						<Link href="/sign-in">Sign in</Link>
 					</Button>
+				)}
 
-					<ClerkLoading>
-						<Skeleton className="h-8 w-8 bg-slate-400 rounded-full" />
-					</ClerkLoading>
+				{isSignedIn && (
+					<div className="flex flex-row items-center">
+						<Button variant="link">
+							<Link href="/dashboard">Dashboard</Link>
+						</Button>
 
-					<ClerkLoaded>
-						<UserButton />
-					</ClerkLoaded>
-				</div>
-			)}
+						<ClerkLoading>
+							<Skeleton className="h-8 w-8 bg-slate-400 rounded-full" />
+						</ClerkLoading>
+
+						<ClerkLoaded>
+							<UserButton />
+						</ClerkLoaded>
+					</div>
+				)}
+				<ModeToggle />
+			</div>
 		</div>
 	);
 }
