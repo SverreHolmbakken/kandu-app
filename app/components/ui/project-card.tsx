1a1f39533c8d9cc9e9d3dbc4d012c63d7c9d950f
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-10-11 16:54:15 +0200

    Feature/project creation db (#36)
    * Changed task title variable name
    
    * Added post and get requests for projects
    
    * Added randomly generated project color

--- app/components/ui/project-card.tsx
+++ app/components/ui/project-card.tsx
@@ -1,18 +1,18 @@
 import { CalendarDays } from "lucide-react";
 
 export default function ProjectCard(props: any) {
-	const { title, description, date } = props;
+	const { name, description, date, color } = props;
+
+	const elementStyle = "flex w-full h-3/4 bg-[" + color + "]";
 
 	return (
 		<button className="flex flex-col h-80 rounded-md border-solid border-[1px] border-secondaryLight hover:opacity-90">
-			<div className="flex w-full h-3/4 bg-black" />
+			<div className={elementStyle} />
 			<div className="flex flex-col w-full text-start p-1">
 				<h2 className="text-mediumFont font-semibold text-primaryDark">
-					{title}
+					{name}
 				</h2>
-				<p className="text-smallFont text-secondaryDark">
-					{description}
-				</p>
+				<p className="text-smallFont text-secondaryDark">{description}</p>
 				<span className="flex flex-row gap-2 text-smallFont h-4 mt-1">
 					<CalendarDays className="h-full w-4 text-secondaryDark" />
 					<p className="text-center text-secondaryDark">{date}</p>
