090827e2dc8f27d32a9371e1a8bb6e624e7fb355
0yvz <oyvindg@live.no>
2023-10-11 11:26:32 +0200

    Adjusted overflow and padding

--- app/components/layout/kanban/kanban-column.tsx
+++ app/components/layout/kanban/kanban-column.tsx
@@ -4,18 +4,26 @@ import TaskModal from "./task-modal";
 
 export default function KanbanColumn() {
 	return (
-		<div className="rounded-md border w-1/4 h-full">
-			<div className="h-12 px-4 text-left align-middle border-b font-medium text-slate-600 dark:text-slate-400 flex items-center justify-between">
-				<div className="mr-2">Column name</div>
-				<div className="ml-auto flex items-center">
-					<TaskModal />
+		<div className="rounded-md border w-1/4 h-full overflow-hidden">
+			<header className="bg-slate-50 sticky top-0 z-10">
+				<div className="h-12 px-4 text-left align-middle border-b font-medium text-slate-600 dark:text-slate-400 flex items-center justify-between">
+					<div className="mr-2">Column name</div>
+					<div className="ml-auto flex items-center">
+						<TaskModal />
+					</div>
+					<div className="ml-2 flex items-center">
+						<EditColumn />
+					</div>
 				</div>
-				<div className="ml-2 flex items-center">
-					<EditColumn />
-				</div>
-			</div>
-			<div className="h-full transition-colors hover:bg-slate-100/50 dark:hover:bg-slate-800/50">
-				<div className="p-4 align-middle">
+			</header>
+			<div className="h-full transition-colors hover:bg-slate-100/50 dark:hover:bg-slate-800/50 overflow-y-auto">
+				<div className="p-4 pt-0 align-middle">
+					<Task />
+					<Task />
+					<Task />
+					<Task />
+					<Task />
+					<Task />
 					<Task />
 				</div>
 			</div>
