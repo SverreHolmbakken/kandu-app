ee0c7a2519b57f24e08bc3b102ab728c45aaf72a
0yvz <oyvindg@live.no>
2023-10-11 10:51:04 +0200

    Changed form table to div

--- app/components/layout/kanban/kanban-column.tsx
+++ app/components/layout/kanban/kanban-column.tsx
@@ -15,32 +15,20 @@ import TaskModal from "./task-modal";
 export default function KanbanColumn() {
 	return (
 		<div className="rounded-md border w-1/4 h-full">
-			<Table>
-				<TableHeader>
-					<TableRow className="bg-white">
-						<TableHead>
-							<div className="flex items-center justify-between">
-								<div className="flex items-center">
-									<div className="mr-2">Column name</div>
-								</div>
-								<div className="ml-auto">
-									<TaskModal />
-								</div>
-								<div className="ml-2 flex items-center">
-									<EditColumn />
-								</div>
-							</div>
-						</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					<TableRow>
-						<TableCell>
-							<Task />
-						</TableCell>
-					</TableRow>
-				</TableBody>
-			</Table>
+			<div className="h-12 px-4 text-left align-middle border-b font-medium text-slate-600 dark:text-slate-400 flex items-center justify-between">
+				<div className="mr-2">Column name</div>
+				<div className="ml-auto flex items-center">
+					<TaskModal />
+				</div>
+				<div className="ml-2 flex items-center">
+					<EditColumn />
+				</div>
+			</div>
+			<div className="h-full transition-colors hover:bg-slate-100/50 dark:hover:bg-slate-800/50">
+				<div className="p-4 align-middle">
+					<Task />
+				</div>
+			</div>
 		</div>
 	);
 }
