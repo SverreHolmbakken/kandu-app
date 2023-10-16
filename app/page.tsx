58fcedab62d4591bd86109ed5bf090904ce0df5e
0yvz <oyvindg@live.no>
2023-10-16 15:21:48 +0200

    Added hero section to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Main from "./components/layout/main";
+import Image from "next/image";
 import { useAuth } from "@clerk/nextjs";
 
 export default function Home() {
@@ -10,9 +10,21 @@ export default function Home() {
 
 	return (
 		<div className="flex h-screen flex-col justify-between m-auto text-primaryDark">
-			<section className="flex w-full lg:px-36 md:px-8 px-1 h-screen">
-				<Main />
-			</section>
+			<div className=" bg-purple-100 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+			<div className=" bg-blue-200 absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+			<main>
+				<section className="flex w-screen h-screen absolute">
+					<Image
+						src="/kandu-hero-image.png"
+						alt=""
+						width="800"
+						height="800"
+						quality="100"
+						priority={true}
+						className="object-scale-down absolute bottom-0 -right-44 z-10"
+					/>
+				</section>
+			</main>
 		</div>
 	);
 }
