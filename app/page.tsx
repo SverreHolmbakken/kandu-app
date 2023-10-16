7af01826b95829a8f0fcacb8066dea005d27d430
0yvz <oyvindg@live.no>
2023-10-16 21:53:56 +0200

    Adjusted overflow

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
 	console.log(userId);
 
 	return (
-		<div className="flex item center h-screen flex-col overflow-y-hidden">
+		<div className="flex item center h-screen w-screen flex-col">
 			<div className=" bg-purple-100 dark:bg-purple-400/50 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 			<div className=" bg-blue-200 dark:bg-blue-400/20 absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 			<main>
-				<section className="flex w-screen h-full absolute">
+				<section className="flex w-full h-full absolute overflow-x-hidden">
 					<div className="text-5xl font-extrabold w-[50vw] m-20">
 						<motion.span
 							className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"
