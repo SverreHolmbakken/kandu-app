bbfc51c7937775ec55c46d46c76d49c719fd712d
Øyvind Gjersum <110482909+oyvindGg@users.noreply.github.com>
2023-10-16 23:58:57 +0200

    Feature/landing page (#45)
    * Added logo for darkmode
    
    * Added background color to header
    
    * removed landing page route
    
    * Added hero section to landing page
    
    * Added hero image
    
    * Installed framer motion
    
    * Added motion to hero elements
    
    * Adjusted overflow
    
    * Update page.tsx
    
    Adjusted width and typo
    
    ---------
    
    Co-authored-by: Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -10,7 +10,7 @@ function Header() {
 	const { isSignedIn } = useAuth();
 
 	return (
-		<div className="flex flex-row w-full justify-between items-center py-2 lg:px-36 md:px-8 px-1 bg-transparent">
+		<div className="flex flex-row w-full justify-between items-center py-2 lg:px-36 md:px-8 px-1 bg-slate-50 dark:bg-zinc-950">
 			<Link href="/" className="flex flex-row items-center gap-6">
 				<img src="/kandu-logo.svg" alt="" />
 				<h1 className="text-primaryDark font-bold text-3xl dark:text-white">
