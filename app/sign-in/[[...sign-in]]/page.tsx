3a88774041f2a59ee59a645faec03f0697eff2b1
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-10-20 16:17:06 +0200

    Added redirections

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
 	return (
 		<div className="flex items-center justify-center h-[80vh]">
-			<SignIn />
+			<SignIn redirectUrl="/dashboard" />
 		</div>
 	);
 }
