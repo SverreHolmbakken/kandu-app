e9bb12c26818f003edf2f3c885623f778e70c2a0
0yvz <oyvindg@live.no>
2023-10-23 12:53:27 +0200

    Made hero section component

---
+++ app/components/landing-page/hero.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { ChevronDownSquare } from "lucide-react";
+
+export default function Hero() {
+	return (
+		<section className="h-screen">
+			<div className="text-5xl font-extrabold w-[50vw] m-20">
+				<motion.span
+					className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"
+					initial={{ opacity: 0, scale: 0 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{
+						type: "spring",
+						delay: 0,
+						duration: 0.7,
+					}}
+				>
+					Kandu
+				</motion.span>
+				<motion.span
+					initial={{ opacity: 0, y: 100 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{
+						type: "spring",
+						delay: 0.1,
+						duration: 3,
+					}}
+				>
+					{" "}
+					is an app developed by newgrads in need of a job.
+				</motion.span>
+			</div>
+			<motion.div
+				className="object-contain absolute bottom-0 -right-44 z-10"
+				initial={{ x: 700, y: -300 }}
+				animate={{ x: 0, y: -80 }}
+				transition={{
+					type: "spring",
+					duration: 0.7,
+				}}
+			>
+				<Image
+					src="/kandu-hero-image.png"
+					alt=""
+					width="800"
+					height="800"
+					quality="100"
+					priority={true}
+					className=""
+				/>
+			</motion.div>
+			<div className="flex absolute justify-center bottom-0 left-1/2 animate-bounce z-20">
+				<ChevronDownSquare className="text-slate-700/50 w-10 h-10" />
+			</div>
+		</section>
+	);
+}
