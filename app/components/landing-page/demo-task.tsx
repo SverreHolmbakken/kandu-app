6608aa039a7aa6f83bed8b3eff65e26b043fefb1
0yvz <oyvindg@live.no>
2023-10-23 16:48:47 +0200

    Created demo-task

--- app/components/landing-page/demo-task.tsx
+++ app/components/landing-page/demo-task.tsx
@@ -1,16 +1,85 @@
-import React from "react";
+import { useState } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
-interface DemoTaskProps {
-	taskId: string;
-	taskContent: string;
-}
+export type Id = string | number;
 
-const DemoTask = ({ taskId, taskContent }: DemoTaskProps) => {
-	return (
-		<div key={taskId} className="border p-2 rounded my-2 h-28">
-			{taskContent}
-		</div>
-	);
+export type Column = {
+	id: Id;
+	title: string;
 };
 
-export default DemoTask;
+export type Task = {
+	id: Id;
+	columnId: Id;
+	content: string;
+};
+interface Props {
+	task: Task;
+}
+
+function TaskCard({ task }: Props) {
+	const [mouseIsOver, setMouseIsOver] = useState(false);
+	const [editMode, setEditMode] = useState(true);
+
+	const {
+		setNodeRef,
+		attributes,
+		listeners,
+		transform,
+		transition,
+		isDragging,
+	} = useSortable({
+		id: task.id,
+		data: {
+			type: "Task",
+			task,
+		},
+		disabled: editMode,
+	});
+
+	const style = {
+		transition,
+		transform: CSS.Transform.toString(transform),
+	};
+
+	const toggleEditMode = () => {
+		setEditMode((prev) => !prev);
+		setMouseIsOver(false);
+	};
+
+	if (isDragging) {
+		return (
+			<div
+				ref={setNodeRef}
+				style={style}
+				className="
+        opacity-30 p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl cursor-grab relative
+      "
+			/>
+		);
+	}
+
+	return (
+		<div
+			ref={setNodeRef}
+			style={style}
+			{...attributes}
+			{...listeners}
+			onClick={toggleEditMode}
+			className="border p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-purple-700 cursor-grab relative task"
+			onMouseEnter={() => {
+				setMouseIsOver(true);
+			}}
+			onMouseLeave={() => {
+				setMouseIsOver(false);
+			}}
+		>
+			<p className="text-black my-auto h-[90%] w-full overflow-x-hidden">
+				{task.content}
+			</p>
+		</div>
+	);
+}
+
+export default TaskCard;
