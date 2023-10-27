b8fdacd81a78ec8c2b5d7806e23bd52c9186912c
0yvz <oyvindg@live.no>
2023-10-27 13:18:07 +0200

    Adjusted section height

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -5,7 +5,7 @@ import { ArrowBigDownDash } from "lucide-react";
 
 export default function Hero() {
 	return (
-		<section className="h-screen relative">
+		<section className="h-[calc(100vh-82px)] relative">
 			<div className="text-5xl font-extrabold w-[50vw] m-20">
 				<motion.span
 					className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"
@@ -32,6 +32,20 @@ export default function Hero() {
 					is an app developed by newgrads in need of a job.
 				</motion.span>
 			</div>
+
+			<div className="ml-20 absolute bottom-28 w-[30vw] h-[18vh] p-4 text-gray-800 bg-white rounded-lg shadow dark:bg-zinc-700/50 dark:text-white">
+				<div className="mb-2 h-full flex">
+					<div className="h-3 text-3xl place-self-start ">“</div>
+					<p className="px-4 text-md  dark:text-white self-center justify-self-center">
+						Technologies used in this app: Next.js, TypeScript, Supabase,
+						Clerk, TailwindCSS, Framer Motion and more.
+					</p>
+					<div className="h-3 text-3xl place-self-end text-gray-600 dark:text-white">
+						”
+					</div>
+				</div>
+			</div>
+
 			<motion.div
 				className="object-contain absolute bottom-0 -right-44 z-10"
 				initial={{ x: 700, y: -300 }}
@@ -51,7 +65,7 @@ export default function Hero() {
 					className=""
 				/>
 			</motion.div>
-			<ArrowBigDownDash className="absolute bottom-24 left-1/2 animate-bounce opacity-40" />
+			<ArrowBigDownDash className="absolute bottom-5 left-1/2 animate-bounce opacity-40" />
 		</section>
 	);
 }
