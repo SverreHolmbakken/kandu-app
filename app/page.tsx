235f971c934e7dc6501ad270525bbb85b0da355b
0yvz <oyvindg@live.no>
2023-10-27 12:42:31 +0200

    Removed absolute from page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { useAuth } from "@clerk/nextjs";
 import Hero from "./components/landing-page/hero";
 import Demo from "./components/landing-page/demo";
 import About from "./components/landing-page/about";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col w-full h-full absolute overflow-x-hidden">
+		<div className="flex flex-col w-full h-full overflow-x-hidden">
 			<div className=" bg-purple-100 dark:bg-purple-400/50 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 			<div className=" bg-blue-200 dark:bg-blue-400/20 absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 			<main className="flex flex-col justify-between">
