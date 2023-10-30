7ecf9a1dd1bf9076d4d253e8389b6e6faebd1ef2
0yvz <oyvindg@live.no>
2023-10-31 00:45:07 +0100

    Changed heading and added blurred colors

--- app/components/landing-page/about.tsx
+++ app/components/landing-page/about.tsx
@@ -5,9 +5,14 @@ import { authors } from "@/app/lib/authordata";
 export default function About() {
 	const authorList = authors;
 	return (
-		<section className="h-screen">
-			<div className="ml-20 text-3xl">Get in touch</div>
-			<div className="flex justify-center items-center">
+		<section className="relative h-screen mt-20 overflow-hidden">
+			<div className="ml-20">
+				<span className="text-4xl font-bold ">Get in touch with us</span>
+				<p className="text-l text-zinc-700">
+					Feel free to contact us via the links below.
+				</p>
+			</div>
+			<div className="flex justify-center items-center gap-11 h-3/4">
 				{authorList.map((author) => (
 					<ContactCard
 						name={author.name}
@@ -20,6 +25,8 @@ export default function About() {
 					/>
 				))}
 			</div>
+			<div className=" bg-blue-200 dark:bg-blue-400/20absolute bottom-0 right-0 overflow-hidden h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] -z-10"></div>
+			<div className="absolute bottom-0 right-0 bg-purple-100 dark:bg-purple-400/50 overflow-hidden h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] -z-10"></div>
 		</section>
 	);
 }
