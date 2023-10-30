3036bc1bfe2ad793f3b61cf2c38103714ae134b9
0yvz <oyvindg@live.no>
2023-10-30 17:43:01 +0100

    Created data for contact cards

---
+++ app/lib/authordata.ts
@@ -0,0 +1,22 @@
+export const authors = [
+	{
+		name: "Sverre Holmbakken",
+		avatar:
+			"https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/sign/images/profilePicture-Sverre.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvcHJvZmlsZVBpY3R1cmUtU3ZlcnJlLnBuZyIsImlhdCI6MTY5ODQxMjMxMCwiZXhwIjoyMDEzNzcyMzEwfQ.HnHwT2U2ZUbUVdXVWJktR7H5P56FLCOfBgm1fJ7rPQg",
+		title: "Frontend developer",
+		github: "",
+		linkedin: "",
+		cv: "",
+		mail: "",
+	},
+	{
+		name: "Øyvind Gjersum",
+		avatar:
+			"https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/sign/images/headshot2.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvaGVhZHNob3QyLmpwZyIsImlhdCI6MTY5ODQxMjMzMywiZXhwIjo0ODIwNDc2MzMzfQ.qI0N3fEdBAMuF_1Lxlvi90rEVwzbBv1VHLiCEaP9tnI&t=2023-10-27T13%3A12%3A13.128Z",
+		title: "Frontend developer",
+		github: "",
+		linkedin: "",
+		cv: "",
+		mail: "",
+	},
+];
