8064dc82a16714cbdf3f6ce52006d91d3376ae84
0yvz <oyvindg@live.no>
2023-10-31 00:45:16 +0100

    Added links to data

--- app/lib/authordata.ts
+++ app/lib/authordata.ts
@@ -4,8 +4,8 @@ export const authors = [
 		avatar:
 			"https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/sign/images/profilePicture-Sverre.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvcHJvZmlsZVBpY3R1cmUtU3ZlcnJlLnBuZyIsImlhdCI6MTY5ODQxMjMxMCwiZXhwIjoyMDEzNzcyMzEwfQ.HnHwT2U2ZUbUVdXVWJktR7H5P56FLCOfBgm1fJ7rPQg",
 		title: "Frontend developer",
-		github: "",
-		linkedin: "",
+		github: "https://github.com/SverreHolmbakken",
+		linkedin: "https://www.linkedin.com/in/sverre-holmbakken/",
 		cv: "",
 		mail: "",
 	},
@@ -14,9 +14,9 @@ export const authors = [
 		avatar:
 			"https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/sign/images/headshot2.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvaGVhZHNob3QyLmpwZyIsImlhdCI6MTY5ODQxMjMzMywiZXhwIjo0ODIwNDc2MzMzfQ.qI0N3fEdBAMuF_1Lxlvi90rEVwzbBv1VHLiCEaP9tnI&t=2023-10-27T13%3A12%3A13.128Z",
 		title: "Frontend developer",
-		github: "",
-		linkedin: "",
+		github: "https://github.com/oyvindGg",
+		linkedin: "www.linkedin.com/in/øyvindgjersum/",
 		cv: "",
-		mail: "",
+		mail: "oyvindg@live.no",
 	},
 ];
