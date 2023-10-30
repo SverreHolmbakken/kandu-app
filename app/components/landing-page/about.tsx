883fb66fa81a84f488a07ab762f6599177ebe03b
0yvz <oyvindg@live.no>
2023-10-30 17:42:48 +0100

    Added contect card to section

--- app/components/landing-page/about.tsx
+++ app/components/landing-page/about.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import ContactCard from "./about-contact-card";
+import { authors } from "@/app/lib/authordata";
 
 export default function About() {
+	const authorList = authors;
 	return (
 		<section className="h-screen">
 			<div className="ml-20 text-3xl">Get in touch</div>
+			<div className="flex justify-center items-center">
+				{authorList.map((author) => (
+					<ContactCard
+						name={author.name}
+						avatar={author.avatar}
+						title={author.title}
+						github={author.github}
+						linkedin={author.linkedin}
+						cv={author.cv}
+						mail={author.mail}
+					/>
+				))}
+			</div>
 		</section>
 	);
 }
