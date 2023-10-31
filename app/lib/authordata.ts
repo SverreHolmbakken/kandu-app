6aa597dd5c13b2d28ff7b0c486242ff9139c00b5
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-10-31 14:34:21 +0100

    Update authordata.ts

--- app/lib/authordata.ts
+++ app/lib/authordata.ts
@@ -1,13 +1,12 @@
 export const authors = [
 	{
 		name: "Sverre Holmbakken",
-		avatar:
-			"https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/sign/images/profilePicture-Sverre.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvcHJvZmlsZVBpY3R1cmUtU3ZlcnJlLnBuZyIsImlhdCI6MTY5ODQxMjMxMCwiZXhwIjoyMDEzNzcyMzEwfQ.HnHwT2U2ZUbUVdXVWJktR7H5P56FLCOfBgm1fJ7rPQg",
+		avatar: "https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/public/images/profilePicture-Sverre.png?t=2023-10-31T13%3A33%3A12.670Z",
 		title: "Frontend developer",
 		github: "https://github.com/SverreHolmbakken",
 		linkedin: "https://www.linkedin.com/in/sverre-holmbakken/",
-		cv: "",
-		mail: "",
+		cv: "https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/public/cvs/CV%20Sverre%20Holmbakken%2010-23.pdf?t=2023-10-31T13%3A32%3A06.477Z",
+		mail: "sverre@holmbakken.no",
 	},
 	{
 		name: "Øyvind Gjersum",
@@ -15,8 +14,17 @@ export const authors = [
 			"https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/sign/images/headshot2.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvaGVhZHNob3QyLmpwZyIsImlhdCI6MTY5ODQxMjMzMywiZXhwIjo0ODIwNDc2MzMzfQ.qI0N3fEdBAMuF_1Lxlvi90rEVwzbBv1VHLiCEaP9tnI&t=2023-10-27T13%3A12%3A13.128Z",
 		title: "Frontend developer",
 		github: "https://github.com/oyvindGg",
-		linkedin: "www.linkedin.com/in/øyvindgjersum/",
+		linkedin: "https://www.linkedin.com/in/%C3%B8yvindgjersum/",
 		cv: "",
 		mail: "oyvindg@live.no",
 	},
+	{
+		name: "Camilla Meskestad Nysterud",
+		avatar: "https://hjojgyofdixsdnxbualg.supabase.co/storage/v1/object/public/images/Nysterud_Camilla-sh.jpg?t=2023-10-31T13%3A27%3A36.593Z",
+		title: "Frontend developer",
+		github: "https://github.com/camillamn",
+		linkedin: "https://www.linkedin.com/in/camilla-meskestad-nysterud/",
+		cv: "",
+		mail: "",
+	}
 ];
