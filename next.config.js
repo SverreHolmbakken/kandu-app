e64d73d9187bbbd233a420deeac8b06e6909d851
Øyvind Gjersum <110482909+oyvindGg@users.noreply.github.com>
2023-10-31 19:23:38 +0100

    Feature/contact information (#57)
    * Added host for supabase images
    
    * Created contect card
    
    * Removed unused section
    
    * Added contect card to section
    
    * Created data for contact cards
    
    * Removed scrollbars
    
    * Added links and adjusted size
    
    * Changed heading and added blurred colors
    
    * Added links to data
    
    * Fix syntax error
    
    * Update authordata.ts
    
    * Update authordata
    
    * Exclude mac from custom scrollbar
    
    ---------
    
    Co-authored-by: Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'hjojgyofdixsdnxbualg.supabase.co',
+				port: '',
+			},
+		],
+	},
 	experimental: {
 		swcPlugins: [["@swc-jotai/react-refresh", {}]],
 	},
