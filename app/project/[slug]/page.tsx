d6081833b9a611c322daafcf4feb4eb4de7ba362
0yvz <oyvindg@live.no>
2023-11-10 12:38:21 +0100

    Add drag and drop functionality for Kanban board columns

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -1,18 +1,32 @@
 "use client";
 
+import KanbanColumn from "@/app/components/layout/kanban/column/column";
+import ProjectNav from "@/app/components/layout/kanban/project/project-nav";
+import NewColumn from "@/app/components/layout/kanban/column/new-column";
+import Task from "@/app/components/layout/kanban/task/task";
+import { Skeleton } from "@/app/components/ui/skeleton";
+
 import { FC, useEffect, useMemo, useState } from "react";
+import { supabaseClient } from "@/app/utils/supabase-client";
 import {
 	getColumnsBySlug,
 	getProjectBySlug,
 } from "@/app/utils/supabase-request";
 import { useAuth } from "@clerk/nextjs";
-import KanbanColumn from "@/app/components/layout/kanban/column/column";
-import ProjectNav from "@/app/components/layout/kanban/project/project-nav";
-import NewColumn from "@/app/components/layout/kanban/column/new-column";
-import { ColumnType } from "@/Types";
-import { Skeleton } from "@/app/components/ui/skeleton";
-import { DndContext } from "@dnd-kit/core";
-import { SortableContext } from "@dnd-kit/sortable";
+import { ColumnType, TaskType } from "@/Types";
+import {
+	DndContext,
+	DragOverlay,
+	DragStartEvent,
+	DragEndEvent,
+	PointerSensor,
+	useSensors,
+	useSensor,
+} from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
+import { Token } from "@clerk/nextjs/server";
+import { any } from "zod";
 
 interface Props {
 	params: { slug: string };
@@ -24,16 +38,24 @@ const Page: FC<Props> = ({ params }) => {
 	const [columns, setColumns] = useState<ColumnType[]>([]);
 	const [project, setProject] = useState<Project | null>(null);
 	const [loading, setLoading] = useState(true);
+	const [columnsUpdated, setColumnsUpdated] = useState(false);
 
 	const columnsId = useMemo(
 		() => columns.map((col) => col.column_id),
 		[columns]
 	);
 
+	const [tasks, setTasks] = useState<TaskType[]>();
+
+	const [activeColumn, setActiveColumn] = useState<ColumnType | null>(null);
+	const [activeTask, setActiveTask] = useState<TaskType | null>(null);
+
 	interface Project {
 		name: string;
 		id: number;
 	}
+
+	console.log("Columns:", columns); // Debug log
 
 	useEffect(() => {
 		const loadColumns = async () => {
@@ -45,10 +67,10 @@ const Page: FC<Props> = ({ params }) => {
 			});
 			setColumns(columns || []);
 			setLoading(false);
+			setColumnsUpdated(false); // Set columnsUpdated to false after fetching the columns
 		};
 		loadColumns();
-	}, []);
-	console.log(columns, "columns");
+	}, [columnsUpdated]); // Add columnsUpdated as a dependency
 
 	useEffect(() => {
 		const loadProject = async () => {
@@ -71,15 +93,27 @@ const Page: FC<Props> = ({ params }) => {
 	}
 
 	const sortedColumns = columns.sort((a, b) => {
-		const dateA = new Date(a?.created_at);
-		const dateB = new Date(b?.created_at);
-		return dateA.getTime() - dateB.getTime();
+		const objectA = a?.order ?? 0;
+		const objectB = b?.order ?? 0;
+		return objectA - objectB;
 	});
+
+	const sensors = useSensors(
+		useSensor(PointerSensor, {
+			activationConstraint: {
+				distance: 3,
+			},
+		})
+	);
 
 	return (
 		<div className="">
 			<ProjectNav title={project?.name} />
-			<DndContext>
+			<DndContext
+				sensors={sensors}
+				onDragStart={onDragStart}
+				onDragEnd={onDragEnd}
+			>
 				<div className="h-[85vh] overflow-x-scroll flex flex-row space-x-5 mx-5">
 					{loading ? (
 						<div className="flex flex-row space-x-5">
@@ -89,7 +123,7 @@ const Page: FC<Props> = ({ params }) => {
 						</div>
 					) : (
 						<SortableContext items={columnsId}>
-							{sortedColumns.sort(compareNumbers).map((column) => (
+							{sortedColumns.map((column) => (
 								<KanbanColumn
 									key={column.id}
 									name={column.column_name}
@@ -106,12 +140,82 @@ const Page: FC<Props> = ({ params }) => {
 						setColumns={setColumns}
 					/>
 				</div>
+
+				{createPortal(
+					<DragOverlay>
+						{activeColumn && (
+							<KanbanColumn
+								name={activeColumn.column_name}
+								columnId={activeColumn.column_id}
+								setColumns={setColumns}
+							/>
+						)}
+						{activeTask && <Task task={activeTask} />}
+					</DragOverlay>,
+					document.body
+				)}
 			</DndContext>
 		</div>
 	);
 
-	function onDragStart(event: OnDragStartEvent) {
-		console.log("Drag start", event);
+	function onDragStart(event: DragStartEvent) {
+		if (event.active.data.current?.type === "Column") {
+			setActiveColumn(event.active.data.current.column);
+			return;
+		}
+		if (event.active.data.current?.type === "Task") {
+			setActiveTask(event.active.data.current.task);
+			return;
+		}
+	}
+
+	async function onDragEnd(event: DragEndEvent) {
+		setActiveColumn(null);
+		setActiveTask(null);
+
+		const { active, over } = event;
+		if (active && over && active.id !== over.id) {
+			if (active.data.current?.type === "Column") {
+				const from = columns.findIndex(
+					(col) => col.column_id === active.id
+				);
+				const to = columns.findIndex((col) => col.column_id === over.id);
+
+				if (from !== -1 && to !== -1) {
+					const updatedColumns = arrayMove(columns, from, to);
+
+					const token = await getToken({ template: "supabase" });
+					const supabase = await supabaseClient(token ?? "");
+
+					for (let i = 0; i < updatedColumns.length; i++) {
+						const column = updatedColumns[i];
+						console.log(
+							`Updating column_id: ${column.column_id}, order: ${i}`
+						); // Debug log
+
+						// Log the column object before the update
+						console.log("Column before update:", column);
+
+						const { data, error } = await supabase
+							.from("columns")
+							.update({ order: i })
+							.eq("column_id", column.column_id);
+
+						if (error) {
+							console.error("Failed to update column order:", error);
+						} else {
+							// Log the updated column data
+							console.log("Updated column data:", data);
+						}
+					}
+
+					setColumns(updatedColumns);
+					setColumnsUpdated(true);
+				}
+			} else if (active.data.current?.type === "Task") {
+				// Handle task drag and drop
+			}
+		}
 	}
 };
 
